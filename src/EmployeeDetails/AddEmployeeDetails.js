9c446250431aaf6a87109400695d07a866ad673c
Priyadarshan <priyadarshan.kumar@softsuave.org>
2022-10-13 22:54:35 +0530

    mui modal added

--- src/EmployeeDetails/AddEmployeeDetails.js
+++ src/EmployeeDetails/AddEmployeeDetails.js
@@ -1,21 +1,56 @@
 import React, { useState } from "react";
 import "./Empstyle.css";
+import { departments } from "./const";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
-function AddEmployeeDetails(props) {
-  const [detailedFormData, setDetailedFormData] = useState({
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function AddEmployeeDetails(props) {
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => setOpen(false);
+  const emptyEmpData = {
     id: "",
     dept: "",
     name: "",
     email: "",
     contact: "",
     address: "",
-  });
+  };
+  const [detailedFormData, setDetailedFormData] = useState(emptyEmpData);
   const [detailedData, setDetailedData] = useState([]);
+  console.log(props.empData[0].name);
+
+  React.useEffect(() => {
+    if (props.empData[0].name) {
+      setDetailedFormData((prev) => {
+        return { ...prev, name: props.empData[0].name };
+      });
+    }
+  }, []);
 
   const addID = (e) => {
+    setDetailedFormData((prev) => {
+      return { ...prev, id: e.target.value };
+    });
     console.log("id value", e.target.value);
   };
   const addDepartment = (e) => {
+    setDetailedFormData((prev) => {
+      return { ...prev, dept: e.target.value };
+    });
     console.log("dept value", e.target.value);
   };
   const addName = (e) => {
@@ -26,77 +61,129 @@ function AddEmployeeDetails(props) {
     console.log("name value", detailedFormData);
   };
   const addEmail = (e) => {
+    setDetailedFormData((prev) => {
+      return { ...prev, email: e.target.value };
+    });
     console.log("email value", e.target.value);
   };
   const addContact = (e) => {
+    setDetailedFormData((prev) => {
+      return { ...prev, contact: e.target.value };
+    });
     console.log("contact value", e.target.value);
   };
   const addAddress = (e) => {
+    setDetailedFormData((prev) => {
+      return { ...prev, address: e.target.value };
+    });
     console.log("address value", e.target.value);
   };
 
+  const saveEmpData = () => {
+    setOpen(true);
+    if (detailedFormData.name && detailedFormData.dept) {
+      setDetailedData((prev) => {
+        return [...prev, detailedFormData];
+      });
+    }
+    setDetailedFormData((prev) => {
+      return { ...prev, emptyEmpData };
+    });
+  };
+
   return (
-    <div className="inputfield">
-      <div className="inputBox">
-        <label>ID</label>
-        <input
-          type="text"
-          // placeholder={props.dept}
-          value={detailedFormData.id}
-          onChange={addID}
-        />
-      </div>
+    <div>
+      <div className="inputfield">
+        <div className="inputBox">
+          <label>ID</label>
+          <input
+            type="text"
+            // placeholder={props.dept}
+            value={detailedFormData.id}
+            onChange={addID}
+          />
+        </div>
 
-      <div className="inputBox">
-        <label>Department</label>
-        <input
-          type="text"
-          value={detailedFormData.dept}
-          onChange={addDepartment}
-        />
-      </div>
+        <div className="inputBox">
+          Department
+          <select value={detailedFormData.dept} onChange={addDepartment}>
+            {departments.map((val, index) => (
+              <option key={index} value={val.DepartmentId}>
+                {val.DepartmentName}
+              </option>
+            ))}
+          </select>
+        </div>
 
-      <div className="inputBox">
-        <label>Name</label>
-        <input
-          type="text"
-          // placeholder={props.name}
-          value={detailedFormData.name}
-          onChange={addDepartment}
-        />
-      </div>
+        <div className="inputBox">
+          <label>Name</label>
+          <input
+            type="text"
+            // placeholder={props.name}
+            value={detailedFormData.name}
+            onChange={addName}
+          />
+        </div>
 
-      <div className="inputBox">
-        <label>Email</label>
-        <input
-          type="text"
-          placeholder="Enter email here."
-          value={detailedFormData.email}
-          onChange={addDepartment}
-        />
-      </div>
+        <div className="inputBox">
+          <label>Email</label>
+          <input
+            type="text"
+            placeholder="Enter email here."
+            value={detailedFormData.email}
+            onChange={addEmail}
+          />
+        </div>
 
-      <div className="inputBox">
-        <label>Contact</label>
-        <input
-          type="text"
-          placeholder="Enter contact number"
-          value={detailedFormData.contact}
-          onChange={addDepartment}
-        />
-      </div>
+        <div className="inputBox">
+          <label>Contact</label>
+          <input
+            type="text"
+            placeholder="Enter contact number"
+            value={detailedFormData.contact}
+            onChange={addContact}
+          />
+        </div>
 
-      <div className="inputBox">
-        <label>Address</label>
-        <textarea
-          type="text"
-          placeholder="Enter city name."
-          value={detailedFormData.address}
-          onChange={addDepartment}
-        />
+        <div className="inputBox">
+          <label>Address</label>
+          <textarea
+            type="text"
+            placeholder="Enter city name."
+            value={detailedFormData.address}
+            onChange={addAddress}
+          />
+        </div>
+        <button className="btn2" onClick={saveEmpData}>
+          Save
+        </button>
       </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <div>
+            {detailedData.map((item, index) => {
+              let selectedObj = departments.find((i) => {
+                return i.DepartmentId == item.dept;
+              });
+              return (
+                <div key={index} className="data">
+                  <div>Id:{item.id}</div>
+                  <div>Name:{item.name}</div>
+                  <div>Department:{selectedObj?.DepartmentName}</div>
+                  <div>Email:{item.email}</div>
+                  <div>Contact:{item.contact}</div>
+                  <div>Address:{item.address}</div>
+                </div>
+              );
+            })}
+          </div>
+        </Box>
+      </Modal>
     </div>
   );
 }
-
-export default AddEmployeeDetails;
--- src/EmployeeDetails/Employee.jsx
+++ src/EmployeeDetails/Employee.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
 import "./Empstyle.css";
 import { departments } from "./const";
+import AddEmployeeDetails from "./AddEmployeeDetails";
 
 function EmployeeData() {
   const [showInput, setShowInput] = useState(false);
   const [formData, setFormData] = useState({ name: "", dept: 30 });
   const [empData, setEmpData] = useState([]);
+  const [showAddButton, setshowAddButton] = useState(false);
+  const [showAddDetails, setShowAddDetails] = useState(false);
 
   const showForm = () => {
     setShowInput(true);
+    setshowAddButton(false);
+    setShowAddDetails(false);
   };
-
+  {
+    console.log("EMP DATA", formData);
+  }
   const addName = (e) => {
     setFormData((prev) => {
       return { ...prev, name: e.target.value };
@@ -34,7 +41,17 @@ function EmployeeData() {
     }
   };
 
-  const addDetails = () => {};
+  const showDetails = () => {
+    setshowAddButton(true);
+    setShowInput(false);
+    setShowAddDetails(false);
+  };
+
+  const showDetailsField = () => {
+    setShowInput(false);
+    setshowAddButton(true);
+    setShowAddDetails(!showAddDetails);
+  };
   return (
     <div className="maindiv">
       <h4 className="h4">EMPLOYEE DATA</h4>
@@ -47,7 +64,7 @@ function EmployeeData() {
               });
 
               return (
-                <div key={index} className="data" onClick={addDetails}>
+                <div key={index} className="data" onClick={showDetails}>
                   <div>Name:{item.name}</div>
                   <div>Department:{selectedObj?.DepartmentName}</div>
                 </div>
@@ -55,43 +72,59 @@ function EmployeeData() {
             })}
           </div>
 
-          <button className="btn" onClick={showForm}>
+          <button className="btn" onClick={showForm} style={{ width: "100%" }}>
             Add an Employee
           </button>
         </div>
-
-        <div className="div3">
-          <div className="addDetailsBtn">
-            <button className="btn">ADD</button>
-          </div>
-
-          {showInput && (
-            <div className="inputfield">
-              Name
-              <input
-                type="text"
-                placeholder="Enter the name here"
-                value={formData.name}
-                onChange={addName}
-              />
-              Department
-              <select value={formData.dept} onChange={addDepartment}>
-                {departments.map((val, index) => (
-                  <option key={index} value={val.DepartmentId}>
-                    {val.DepartmentName}
-                  </option>
-                ))}
-              </select>
-              <div>
-                <button className="btn2" onClick={() => setShowInput(false)}>
-                  Cancel
-                </button>
-                <button className="btn2" onClick={saveData}>
-                  Save
+        <div className="rightdiv">
+          {showAddButton && (
+            <div>
+              <div className="addDetailsBtn">
+                <button className="btn" onClick={showDetailsField}>
+                  ADD
                 </button>
               </div>
             </div>
           )}
+          {showAddDetails && (
+            <div>
+              <AddEmployeeDetails empData={empData} />
+              {/* <button className="btn2" onClick={() => setShowAddDetails(false)}>
+                Cancel
+              </button> */}
+              {/* <button className="btn2">Save</button> */}
+            </div>
+          )}
+
+          <div className="div3">
+            {showInput && (
+              <div className="inputfield">
+                Name
+                <input
+                  type="text"
+                  placeholder="Enter the name here"
+                  value={formData.name}
+                  onChange={addName}
+                />
+                Department
+                <select value={formData.dept} onChange={addDepartment}>
+                  {departments.map((val, index) => (
+                    <option key={index} value={val.DepartmentId}>
+                      {val.DepartmentName}
+                    </option>
+                  ))}
+                </select>
+                <div>
+                  <button className="btn2" onClick={() => setShowInput(false)}>
+                    Cancel
+                  </button>
+                  <button className="btn2" onClick={saveData}>
+                    Save
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
